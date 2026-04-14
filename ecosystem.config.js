e5deb556c5c324e61569b173be214bf974dbe5d6
snowsun <snowsun@ehsure.com>
2026-04-14 15:42:42 +0800

    chore: 优化部署配置、环境变量模板、PM2配置

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [{
+    name: 'gaoge-server',
+    script: 'dist/main.js',
+    instances: 'max',
+    exec_mode: 'cluster',
+    env: {
+      NODE_ENV: 'production',
+      PORT: process.env.APP_PORT || 3000
+    },
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    combine_logs: true,
+    max_memory_restart: '512M'
+  }]
+}
